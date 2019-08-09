f1497ca70d5b9a7e714862fc07bef6a2e0f29254
Metab0i <metaboi0000@gmail.com>
2019-08-09 19:36:37 +0930

    Added a list-through feature for youtube video search

--- commands/youtube.js
+++ commands/youtube.js
@@ -24,6 +24,12 @@ module.exports = {
       if(assist_func.userTimeOut(msg) == true) return;  
       msg.channel.startTyping();
 
+      //list-through pages var pre-set
+      let pages = [];
+      let page = 1;
+      const backwardsFilter = (reaction, usr) => reaction.emoji.name === '⏪' && usr.id === msg.author.id;
+      const forwardsFilter = (reaction, usr) => reaction.emoji.name === '⏩' && usr.id === msg.author.id;
+
       //grab a request for a video to find and URI
       var query = encodeURIComponent(msg.content.slice(msg.content.indexOf(" "), msg.content.length));
 
@@ -34,11 +40,54 @@ module.exports = {
       };
 
       var result = await rp(options);
-      var link = "https://www.youtube.com/watch?v=" + result.items[0].id.videoId;
+
+      if(result.items.length == 0) return msg.channel.send("`Invalid search, try again.`")
+      
+      //var link = "https://www.youtube.com/watch?v=" + result.items[0].id.videoId;
+      var link = "https://www.youtube.com/watch?v=";
+
+      for(var vod_id in result.items){
+        pages.push("`Command user: " + msg.author.username + "`\n" + link + result.items[vod_id].id.videoId + "\n`page: " + (parseInt(vod_id)+1) + "`");
+      }
 
       msg.channel.stopTyping();
-      msg.channel.send(link);
+      msg.channel.send(pages[0]).then(msg_pages =>{
+        msg_pages.react('⏪').then( r => {
+          msg_pages.react('⏩');
 
+          //create 2 collectors of reactions, set filters ^
+          const backwards = msg_pages.createReactionCollector(backwardsFilter, { time: 120000 });
+          const forwards = msg_pages.createReactionCollector(forwardsFilter, { time: 120000 }); 
+
+          //on action, change the values:
+          backwards.on('collect', r => { 
+            msg_pages.reactions.forEach(function(value){
+              value.remove(msg.author.id);
+            })
+
+            if (page === 1) return; 
+
+            page--; 
+
+            msg_pages.edit(pages[page-1]); 
+          })
+          
+          //on action change the values:
+          forwards.on('collect', r => { 
+            msg_pages.reactions.forEach(function(value){
+              value.remove(msg.author.id);
+            })
+            
+            if (page === pages.length && pages.length != 1) return;
+
+            if (pages.length != 1){
+              page++;
+            } 
+  
+            msg_pages.edit(pages[page-1]); 
+          });
+        });
+      });
     }
   }
 };
