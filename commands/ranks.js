af6a90287511c3ed170e9894070bf63aa4883d36
Metab0i <metaboi0000@gmail.com>
2020-01-24 01:35:25 +1030

    advancing further with ranks implementation

--- commands/ranks.js
+++ commands/ranks.js
@@ -14,8 +14,8 @@ module.exports = {
   ranks_set_up: async function(prefix, msg, pool, client){
     const ranks_exp = new RegExp("^" + prefix + "ranksetup");
 
-    if(ranks_exp.test(msg.content.toLowerCase())){
-
+    //check that the one who runs the command is admin of the server. 
+    if(ranks_exp.test(msg.content.toLowerCase().trim()) && msg.member.hasPermission("ADMINISTATOR") == true){
       //User timer
       if(assist_func.userTimeOut(msg) == true) return;
 
@@ -29,36 +29,47 @@ module.exports = {
       const embed = new Discord.RichEmbed()
         .setTitle("Ranks set-up page:")
         .setDescription("Send a role name you want to track or type cancel.")
-        .setColor(assist_func.random_hex_colour())
-      
-      const set_up_msg = await msg.channel.send(embed)
+        .setColor(assist_func.random_hex_colour());
 
-      msg_collector.on('collect', message => {
-
+      await msg.channel.send(embed);
+      //use collector as a way for waiting on input from the user (integrate it within the roleCollector_loop())
+      msg_collector.once('collect', message => {   
         //define a recursive function to track inputs from a user
         const roleCollector_loop = async function() {
+          embed.setDescription("Would you like to set another role? Type cancel if you wish to exit.");
           
-          //check if such role exists
-          set_up_msg.delete(300);
-          //if exists, proceed to query for how much xp is required to achieve said role and loop again to see if they want to set up another role
-          roleCollector_loop();
+          const new_prompt = await message.channel.send(embed);
 
           if(message.content.toLowerCase() == "cancel"){
             msg_collector.stop();
-
             const msg_confirmation = await msg.channel.send("`Operation cancelled`");
-            msg_confirmation.delete(400);
+            msg_confirmation.delete(1000);
+            new_prompt.delete();
           }
 
-          else if(msg_collector.ended == true){
-            const msg_confirmation = await msg.channel.send("`Time has ran out.`");
-            msg_confirmation.delete(400);
+          let check_role = false;
+
+          //if exists, proceed to query for how much xp is required to achieve said role and loop again to see if they want to set up another role
+          message.guild.roles.forEach(role => {
+            if(role.name.toLowerCase() == message.content.toLowerCase()){
+              new_prompt.delete(300);
+              
+              check_role = true;
+              roleCollector_loop();
+            }
+          });
+
+          if(check_role == false){
+            msg_collector.stop();
+
+            embed.setDescription("Wrong Role Entered. Try Again");
+
+            new_prompt.edit(embed);
+            new_prompt.delete(1000);
           }
         }
-
-        //initiate a loop
-        roleCollector_loop();
         
+        roleCollector_loop();
       })
     }
 
