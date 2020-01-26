e6a5669d4ef4b16341ceb86a0b80a189a8a2ff9b
Metab0i <metaboi0000@gmail.com>
2020-01-27 01:36:58 +1030

    minor changes to ascii_image.js

--- commands/ascii_image.js
+++ commands/ascii_image.js
@@ -9,7 +9,8 @@ module.exports = {
   /**
    * @name ascii_img(...)
    * 
-   * @description : turn any image into ascii text art by grayscaling the image, iterating over every pixel and based on the pixel contrast proceed to add an ascii character to the string
+   * @description : turn any image into ascii text art by grayscaling the image, iterating over every pixel 
+   *                and based on the pixel contrast proceed to add an ascii character to the string
    * 
    * @param {String} prefix 
    * @param {String} msg 
@@ -39,6 +40,7 @@ module.exports = {
       //populate media array with media
       let media_array = [];
 
+      //to give embeds and media attached to messages load prior to using them
       await new Promise(r => setTimeout(r, 2000));
 
       if(msg.attachments.size > 0){
@@ -58,8 +60,7 @@ module.exports = {
       }
 
       if(media_array.length == 0){
-        msg.channel.send("`Need an image for this command to work`");
-        return;
+        return msg.channel.send("`Need an image for this command to work`");
       }
 
       msg.channel.startTyping();
@@ -71,15 +72,12 @@ module.exports = {
 
         //grayscale and resize image
         image.grayscale();
-        image.resize(400, 200)
-
-        //console.log(image.bitmap.height + " " + image.bitmap.width);
+        image.resize(400, 200);
 
         for(let y = 0; y < image.bitmap.height; y+= 1){
           let avg_contrast_mini = [];
 
           for(let x = 0; x < image.bitmap.width; x+= 1){
-            //"#" + Math.floor(image.getPixelColor(x, y)).toString(16))
             const pixel_rgba = Jimp.intToRGBA(image.getPixelColor(x, y))
             const contrast_value = (0.299*pixel_rgba.r + 0.587*pixel_rgba.g + 0.114*pixel_rgba.b).toFixed(1);
             avg_contrast_mini.push(contrast_value);
@@ -102,7 +100,6 @@ module.exports = {
 
               avg_contrast_mini = [];
 
-
               for(var range_mini in contrast_symbol){
                 const range_comparison = range_mini.split(" ");
                 if(avg_cont_fin >= parseFloat(range_comparison[0]) && avg_cont_fin <= parseFloat(range_comparison[1])){
@@ -116,7 +113,6 @@ module.exports = {
           if(y % 7 == 0) {
             ascii_mini += "\n"
           }
-          
         }
         
         //html version
@@ -129,6 +125,7 @@ module.exports = {
           if(err) return console.error('on [' + msg.content + ']\nBy <@' + msg.author.id + ">", err.content);
         });
 
+        //giving operation extra 2s to insure no hiccups occur
         await new Promise(r => setTimeout(r, 2000));
 
         msg.channel.send("`full scale ascii`:", {files: ['../Mora bot/commands/ascii_big.html', '../Mora bot/commands/ascii_big.txt']})
