84d91cabf7df5171709f4e614253943eb6c5ec5a
Metab0i <metaboi0000@gmail.com>
2020-02-06 09:48:10 +1030

    organised reputation.js file

--- commands/reputation.js
+++ commands/reputation.js
@@ -75,6 +75,8 @@ module.exports = {
     }
   },
 
+//- - - - - - - - - - - - - - - - - - - - - - - - - Main functions - - - - - - - - - - - - - - - - - - - - - - - - -
+
   /**
    * @name rep_set_up(...)
    * 
